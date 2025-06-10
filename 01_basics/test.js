41d734c9956cf85cb7978590589f11b3d8b60dff
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2025-06-10 14:03:30 +0000

    changes done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Harshit")
+// let value = 3
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Harshit\t"
+let str2 = "nijhawan"
+// let str3 = str1 + str2
+// console.log(str3)
+console.log(str1 + str2) 
+
